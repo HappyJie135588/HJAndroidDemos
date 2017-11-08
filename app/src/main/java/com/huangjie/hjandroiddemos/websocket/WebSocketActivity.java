80909c071747b2916d0e31394676caf35279f141
HuangJie <361125241@qq.com>
2017-11-08 18:57:55 +0800

    websocket

---
+++ app/src/main/java/com/huangjie/hjandroiddemos/websocket/WebSocketActivity.java
@@ -0,0 +1,15 @@
+package com.huangjie.hjandroiddemos.websocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huangjie.hjandroiddemos.R;
+
+public class WebSocketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_socket);
+    }
+}
